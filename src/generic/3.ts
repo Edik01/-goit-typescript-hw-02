9330fd56daac1527ac905823dccad33f8207a851
Edik Huloian <guloyan.edik@gmail.com>
2023-11-22 20:30:15 -0800

    sumarry

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -1,8 +1,6 @@
-/*
-  У вас є функція merge, яка поєднує два об'єкти. 
-  Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
-*/
 function merge<T, U>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB);
 }
-export {};
+
+// Приклад використання
+const mergedObj = merge({ name: "John" }, { age: 25 });
--- src/generic/5.ts
+++ src/generic/5.ts
@@ -1,7 +1,9 @@
-interface KeyValuePair<K, V> {
+interface KeyValuePair<K extends string | symbol, V> {
   key: K;
   value: V;
 }
 
-const pair1: KeyValuePair<number, string> = { key: 1, value: "one" };
-const pair2: KeyValuePair<string, boolean> = { key: "isTrue", value: true };
+// Приклади використання
+const pair1: KeyValuePair<number, string> = { key: 1, value: "one" }; // Помилка, оскільки key має бути строкою або символом
+const pair2: KeyValuePair<string, boolean> = { key: "isTrue", value: true }; // Ок, так як key - строка
+const pair3: KeyValuePair<symbol, number> = { key: Symbol(), value: 42 }; // Ок, так як key - символ
